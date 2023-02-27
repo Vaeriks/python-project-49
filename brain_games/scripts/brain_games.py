4176d291bb1f393976dcc0e08773e292975c2b67
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-02-27 21:26:15 +0700

    update

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,15 @@
 
 from brain_games.cli import welcome_user
 
+
 def brain_games():
-	print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+
 
 def main():
-	brain_games()
-	welcome_user()
+    brain_games()
+    welcome_user()
+
 
 if __name__ == '__main__':
-	main()
+    main()
