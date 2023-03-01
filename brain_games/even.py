8884e41b310e14a34b708901e982b89270687e2f
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-03-01 21:09:35 +0700

    update even.py

--- brain_games/even.py
+++ brain_games/even.py
@@ -18,11 +18,16 @@ def welcome_():
                     number % 2 != 0 and answer_user == 'no'):
                 print('Correct!')
                 i += 1
+                if i == 3:
+                    print('Congratulations, ' + name + '!')
             else:
                 if number % 2 == 0 and answer_user != 'yes':
                     print("'no' is wrong answer ;(. Correct answer was 'yes'.")
+                    print("Let's try again, " + name + '!')
+                    break
                 else:
                     print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-    is_even_games()
+                    print("Let's try again, " + name + '!')
+                    break
 
-    print('Congratulations, ' + name + '!')
+    is_even_games()
