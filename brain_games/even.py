1ca471fb9fe7777d0216ea07ae2879c9fe008d0c
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-03-09 10:07:36 +0700

    update even

--- brain_games/even.py
+++ brain_games/even.py
@@ -5,8 +5,8 @@ import prompt
 def welcome_():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello, ' + name + '!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print(
+        f'Hello, {name}!\nAnswer "yes" if the number is even, otherwise answer "no".')
 
     def is_even_games():
         i = 0
@@ -22,12 +22,12 @@ def welcome_():
                     print('Congratulations, ' + name + '!')
             else:
                 if number % 2 == 0 and answer_user != 'yes':
-                    print("'no' is wrong answer ;(. Correct answer was 'yes'.")
-                    print("Let's try again, " + name + '!')
+                    print(
+                        f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
                     break
                 else:
-                    print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-                    print("Let's try again, " + name + '!')
+                    print(
+                        f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
                     break
 
     is_even_games()
