e53a9e7d19eb0ce844aa30fe35916ef47fe6bb60
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-03-27 20:36:47 +0700

    new file brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-
-from brain_games.even import welcome_
+from brain_games.games import games_
+from brain_games.even import game
 
 
 def main():
-    welcome_()
+    rules = 'Answer "yes" if the number is even, otherwise answer "no"'
+    games_(rules, game)
 
 
 if __name__ == '__main__':
