654d6b36a939f0d87256ec887e5c4157c6b68da1
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-03-27 20:28:50 +0700

    new file games

---
+++ brain_games/games.py
@@ -0,0 +1,20 @@
+import prompt
+
+
+def games_(rules, game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(rules)
+    i = 0
+    while i < 3:
+        answer, answer_user = game()
+        if str(answer) == answer_user:
+            print('Correct!')
+            i += 1
+            if i == 3:
+                print(f"Congratulations, {name}!")
+        else:
+            print(
+                f"{answer_user} is wrong answer ;(. Correct answer was {answer}.\nLet's try again, {name}!")
+            break
