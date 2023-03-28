7452fcacb6ec883620249a58734b3c6e21f5940c
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-03-28 20:21:45 +0700

    new file brain_gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from brain_games.games import games_
+from brain_games.nod import game
+
+
+def main():
+    rules = 'Find the greatest common divisor of given numbers'
+    games_(rules, game)
+
+
+if __name__ == '__main__':
+    main()
