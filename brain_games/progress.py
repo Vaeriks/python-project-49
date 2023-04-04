164e09d704c999acd849e575862158f8ee775197
Vaeriks <kaktus.v.tapkax@gmail.com>
2023-04-04 18:11:31 +0700

    update progress.py

--- brain_games/progress.py
+++ brain_games/progress.py
@@ -10,7 +10,13 @@ def game():
     progress = list(posl)
     i = randint(0, 9)
     answer_1 = progress[i]
-    progress[i] = '..'
-    print(f'Question: {progress}')
+    a = str('..')
+    result = ''
+    for item in progress:
+        if item == progress[i]:
+            result = result + a + ' '
+        else:
+            result = result + str(item) + ' '
+    print(f'Question: {result}')
     answer_user_1 = prompt.string("Your answer: ")
     return answer_1, answer_user_1
